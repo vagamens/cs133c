7bb910c8a4747468c00e4d63475f52ac0be7e6b7
Kevin Clay <clay.kvn.c@gmail.com>
2017-01-30 12:32:05 -0800

    Assignment 2
    completed firelaws.c
    
    Changes to be committed:
    	new file:   Assignment 2/firelaws.c
    	renamed:    Assignment 2/guesser.c -> Assignment 2/numGuess.c

---
+++ Assignment 2/firelaws.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int attendees;
+	int capacity;
+	int difference;
+
+	printf("Enter the number of meeting participants: ");
+	scanf(" %d",&attendees);getchar();
+
+	printf("Enter the meeting room capacity: ");
+	scanf(" %d",&capacity);getchar();
+
+	if(attendees > capacity) {
+		difference = attendees - capacity;
+		printf("\nThe number of attendees is too large to meet fire codes.\n");
+		if(difference == 1) {
+			printf("Please exclude %d attendee.\n", difference);
+		} else {
+			printf("Please exclude %d attendees.\n", difference);
+		}
+	} else {
+		printf("\nYour meeting is good to go. Have a good time.\n");
+	}
+
+	return 0;
+}
