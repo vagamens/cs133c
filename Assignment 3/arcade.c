2c6b2e471f88122c9a7fe150fec880c5e243991b
Kevin Clay <clay.kvn.c@gmail.com>
2017-02-06 16:35:04 -0800

    Assignment 3
    added checking to get the "ones" correct.
    
    Changes to be committed:
    	modified:   Assignment 3/arcade.c

--- Assignment 3/arcade.c
+++ Assignment 3/arcade.c
@@ -8,18 +8,20 @@ int main(void) {
 	int coupForBar = 10;
 	int coupForGum = 3;
 
-	printf("How many coupons do you have?");
+	printf("How many coupons do you have? ");
 	scanf("%d", &coupons);
 	clrBuf();
 
 	if(coupons == 0) {printf("You do not have enough coupons\n");return 0;}
-	candyBars = coupons/10;
-	coupons = coupons - candyBars*10;
-	gumballs = coupons/3;
-	coupons = coupons - gumballs*3;
+	candyBars = coupons/coupForBar;
+	coupons = coupons - candyBars*coupForBar;
+	gumballs = coupons/coupForGum;
+	coupons = coupons - gumballs*coupForGum;
 
-	printf("You can purchase %d candy bars.\n", candyBars);
-	printf("You can purchase %d gumballs.\n", gumballs);
-	if(coupons == 1) {printf("You will have %d coupon left.", coupons);}
+	if(candyBars == 1) {printf("You can purcahse 1 candy bar.\n");}
+	else {printf("You can purchase %d candy bars.\n", candyBars);}
+	if(gumballs == 1) {printf("You can purchase 1 gumball.\n");}
+	else {printf("You can purchase %d gumballs.\n", gumballs);}
+	if(coupons == 1) {printf("You will have %d coupon left.\n", coupons);}
 	else {printf("You will have %d coupons left.", coupons);}
 }
